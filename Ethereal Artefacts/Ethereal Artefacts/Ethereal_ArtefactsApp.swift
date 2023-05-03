af0dbcc4bf67fc2dae761f4108d829e1ca3fb3ce
AlbertEdward <albert.khurshudyan96@gmail.com>
2023-05-03 23:19:31 +0300

    initial

---
+++ Ethereal Artefacts/Ethereal Artefacts/Ethereal_ArtefactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ethereal_ArtefactsApp.swift
+//  Ethereal Artefacts
+//
+//  Created by Albert Khurshudyan on 3.05.23.
+//
+
+import SwiftUI
+
+@main
+struct Ethereal_ArtefactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
